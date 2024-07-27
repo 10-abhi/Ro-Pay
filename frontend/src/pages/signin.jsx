f1a2ae31a94e1c13f8f62c59e14e596d9f3f257b
Abhishek Mishra <abhishek81936@gmail.com>
2024-07-27 20:15:28 +0530

    Updating signin page

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -3,17 +3,30 @@ import { SubHeading } from "../components/subHeading";
 import { InputBox } from "../components/inputbox";
 import { Button } from "../components/button";
 import { BottomWarning } from "../components/bottomWarning";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 export function Signin (){
-    
+      const [username , setUsername] = useState("");
+      const [password , setPassword] = useState("");
+      const navigate = useNavigate();
   return  <div className="flex justify-center bg-slate-400 h-screen">
     <div className="flex flex-col justify-center">
       <div className="w-96 bg-white rounded-lg text-center px-4 p-2 h-max"> 
        
      <Heading label={"Sign In"} ></Heading>
      <SubHeading label={"Enter your credentials to access your account"}></SubHeading>
-     <InputBox label={"Email" } placeholder={"xyz@example.com"}></InputBox>
-     <InputBox label={"Password"} placeholder={"123456"}></InputBox>
-     <Button label={"Sign In"}></Button></div>
+     <InputBox label={"Email" } placeholder={"xyz@example.com"} onChange={e=>{setUsername(e.target.value)}}></InputBox>
+     <InputBox label={"Password"} placeholder={"123456"} onChange={e=>{setPassword(e.target.value)}}></InputBox>
+     <Button label={"Sign In"} onClick={async()=>{
+        const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+           username,
+           password
+         })
+         if(response.status == 200){
+          navigate("/dashboard");
+         }
+     }} ></Button></div>
      <BottomWarning label={"Don't have an account? "} buttonText={"Sign Up"} to={"/signup"}></BottomWarning>
    </div></div> 
 }
